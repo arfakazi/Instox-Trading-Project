2ccfe0941ec88a56a12ac3820295bda5d4d41cd1
zaidvenger <zaidvenger123@outlook.com>
2025-05-20 02:15:53 +0400

    Environmental variables update

--- Scripts/supabaseClient.js
+++ Scripts/supabaseClient.js
@@ -1,6 +1,6 @@
 // supabaseClient.js
-const SUPABASE_URL = CONNECTION_URL;
-const SUPABASE_ANON_KEY = CONNECTION_KEY;
+const SUPABASE_URL = process.env.CONNECTION_URL;
+const SUPABASE_ANON_KEY = process.env.CONNECTION_KEY;
 
 // Only initialize once
 window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
